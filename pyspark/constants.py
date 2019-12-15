ed6939c0d8d0f40abe992b858a4573be64c2ee1f
Villoro <villoro7@gmail.com>
2019-12-15 20:03:01 +0100

    [NEW] pyspark starter code

---
+++ pyspark/constants.py
@@ -0,0 +1,8 @@
+PATH_DATA = "data/"
+
+
+URI_CSV = f"{PATH_DATA}Dades_meteorol_giques_de_la_XEMA.csv"
+URI_DELTA = f"{PATH_DATA}weather_data.delta"
+
+URI_STATIONS = f"{PATH_DATA}Metadades_estacions_meteorol_giques_autom_tiques.csv"
+URI_VARS = f"{PATH_DATA}Metadades_variables_meteorol_giques.csv"

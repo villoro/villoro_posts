25ba440277218902a83601ccd9414acfefde6e6a
Arnau Villoro <arnau@villoro.com>
2021-06-04 17:48:32 +0200

    black the test rg

--- 0037-pyarrow/test_reading_row_group.py
+++ 0037-pyarrow/test_reading_row_group.py
@@ -1,9 +1,10 @@
 from os import walk
 
-from tqdm import tqdm
 import pandas as pd
 import pyarrow.dataset as ds
 import pyarrow.parquet as pq
+
+from tqdm import tqdm
 
 from create_datasets import PATH_PARQUET
 from utils import store_results
@@ -38,16 +39,19 @@ def pyarrow_single_read(path):
         dfs.append(df)
     df = pd.concat(dfs)
 
-    df = df[(df[PARTITION_COL] >= FILTER_VAL) &
-            (df[ROWGROUP_COL] == ROWGROUP_FILTER_VAL)]
+    df = df[(df[PARTITION_COL] >= FILTER_VAL) & (df[ROWGROUP_COL] == ROWGROUP_FILTER_VAL)]
 
     return df.shape
 
 
 def pyarrow_parquet_ds_read(path):
 
-    kwa = {"filters": [(f"p_{PARTITION_COL}", ">=", FILTER_VAL),
-                       (f"{ROWGROUP_COL}", "=", ROWGROUP_FILTER_VAL)]}
+    kwa = {
+        "filters": [
+            (f"p_{PARTITION_COL}", ">=", FILTER_VAL),
+            (f"{ROWGROUP_COL}", "=", ROWGROUP_FILTER_VAL),
+        ]
+    }
     dataset = pq.ParquetDataset(path, validate_schema=False, **kwa)
     df = dataset.read_pandas().to_pandas()
 
@@ -56,8 +60,10 @@ def pyarrow_parquet_ds_read(path):
 
 def pyarrow_ds_read(path):
     dataset = ds.dataset(path, format="parquet", partitioning="hive")
-    table = dataset.to_table(filter=(ds.field(f"p_{PARTITION_COL}") >= FILTER_VAL) &
-                                    (ds.field(ROWGROUP_COL) == ROWGROUP_FILTER_VAL))
+    table = dataset.to_table(
+        filter=(ds.field(f"p_{PARTITION_COL}") >= FILTER_VAL)
+        & (ds.field(ROWGROUP_COL) == ROWGROUP_FILTER_VAL)
+    )
 
     df = table.to_pandas(use_threads=True)
     return df.shape

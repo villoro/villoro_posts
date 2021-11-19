45a8238b1ae44a168015510a6548658633b84dfe
Arnau Villoro <arnau@villoro.com>
2021-11-19 19:03:48 +0100

    Delete repeated example

--- datamesh_example/example_with_files/utils.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger as log
-
-from pyspark.sql import SparkSession
-
-
-def get_spark_session():
-    log.info("Creating Spark Session")
-    return SparkSession.builder.appName("DM").getOrCreate()

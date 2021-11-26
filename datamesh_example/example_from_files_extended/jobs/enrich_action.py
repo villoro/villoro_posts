500ad50620b7104bb87c6987a21b6139cfa1a276
Arnau Villoro <arnau@villoro.com>
2021-11-26 16:37:34 +0100

    Simplify further jobs

--- datamesh_example/example_from_files_extended/jobs/enrich_action.py
+++ datamesh_example/example_from_files_extended/jobs/enrich_action.py
@@ -1,7 +1,3 @@
-from abc import abstractmethod
-from datetime import date
-
-
 from ports.events import OrderCreatedPort
 from ports.live_db import CitiesPort
 from ports.live_db import CustomerSubscriptionsPort
@@ -18,17 +14,6 @@ from utils import get_spark_session
 
 
 class EnrichActionJob(TransformLinearlyJob):
-    def __init__(self, spark, exec_date, n_days):
-        self.spark = spark
-        self.exec_date = exec_date
-        self.n_days = n_days
-
-        # Create the table
-        self.table = self.action_port(spark, exec_date, n_days)
-
-        # Set transformations
-        self.transformations = self.get_transformations()
-
     def get_transformations(self):
         return [
             AddTimezone(CitiesPort(self.spark)),
@@ -39,14 +24,9 @@ class EnrichActionJob(TransformLinearlyJob):
             ),
         ]
 
-    @property
-    @abstractmethod
-    def action_port(self):
-        raise NotImplementedError
-
 
 class EnrichCEOrderCreatedJob(EnrichActionJob):
-    action_port = OrderCreatedPort
+    main_port = OrderCreatedPort
 
     def get_transformations(self):
         return super().get_transformations() + [AddLocalTime()]
--- datamesh_example/example_from_files_extended/jobs/interfaces.py
+++ datamesh_example/example_from_files_extended/jobs/interfaces.py
@@ -1,16 +1,30 @@
-from typing import List
-
-from transformations.interfaces import Table
-from transformations.interfaces import Transformation
+from abc import abstractmethod
+from datetime import date
 
 
 class TransformLinearlyJob:
-    def __init__(self, table: Table, transformations: List[Transformation]):
-        self.table = table
-        self.transformations = transformations
+    def __init__(self, spark, exec_date: date, n_days: int):
+        self.spark = spark
+        self.exec_date = exec_date
+        self.n_days = n_days
+
+        # Create the table
+        self.table = self.main_port(spark, exec_date, n_days)
+
+        # Set transformations
+        self.transformations = self.get_transformations()
 
     def run(self):
         for transformation in self.transformations:
             self.table = transformation._apply(self.table)
 
         self.table.write()
+
+    @abstractmethod
+    def get_transformations(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def main_port(self):
+        raise NotImplementedError
